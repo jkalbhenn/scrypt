8c9154dbd5611705bba07b866e5ff0385ec8072c
Julian Kalbhenn <jkalbhenn@intera.de>
2013-05-28 10:37:39 +0200

    fix install script not creating directories

--- build/make+install.sh
+++ build/make+install.sh
@@ -10,9 +10,9 @@ gcc -shared -O$o -std=c11 -fPIC src/scrypt.c -o temp/$n.so -lm -Wl,--version-scr
 #install libscrypt
 prefix=$1
 t=$prefix/usr/lib/$n.so
-cp temp/$n.so $t && chmod 644 $t
+cp -r temp/$n.so $t && chmod 644 $t
 t=$prefix/usr/include/scrypt.h
-cp src/scrypt.h $t && chmod 644 $t
+cp -r src/scrypt.h $t && chmod 644 $t
 
 #compile scrypt-kdf
 n=scrypt-kdf
@@ -20,4 +20,4 @@ gcc -O$o -std=c11 -lscrypt src/cli.c -o temp/$n && chmod 755 temp/$n
 
 #install scrypt-kdf
 t=$prefix/usr/bin/$n
-cp temp/$n $t && chmod 755 $t
+cp -r temp/$n $t && chmod 755 $t
