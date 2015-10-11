7959aa306b8975b824a722eadc80b8bf59365cf8
jkalbhenn <jkal@posteo.eu>
2015-10-11 10:19:35 +0000

    remove the shared library version script because it is not clear if it is useful. make the install script exit on error

--- exec/make+install.sh
+++ exec/make+install.sh
@@ -1,20 +1,26 @@
 #the binary scrypt-kdf depends on libscrypt, so libscrypt is build and installed first, then scrypt-kdf
 
+exit_on_error() {
+  $* || exit 1
+}
+
 prefix=$1
 mkdir -p temp
 
 echo compile libscrypt
 n=libscrypt
-gcc -shared -O3 -std=c11 -fPIC source/scrypt.c -o temp/$n.so -lm -Wl,--version-script=other/export && chmod 644 temp/$n.so
+exit_on_error gcc -shared -O3 -std=c11 -fPIC -lm -o temp/$n.so source/scrypt.c
+exit_on_error chmod 644 temp/$n.so
 
 echo install libscrypt
-install -m 644 -D temp/$n.so $prefix/usr/lib/$n.so
-install -m 644 -D source/scrypt.h $prefix/usr/include/scrypt.h
+exit_on_error install -m 644 -D temp/$n.so $prefix/usr/lib/$n.so
+exit_on_error install -m 644 -D source/scrypt.h $prefix/usr/include/scrypt.h
 
 echo compile scrypt-kdf
 n=scrypt-kdf
-gcc -O3 -std=c11 -lscrypt source/cli.c -o temp/$n && chmod 755 temp/$n
+exit_on_error gcc -O3 -std=c11 -lscrypt source/cli.c -o temp/$n
+exit_on_error chmod 755 temp/$n
 
 echo install scrypt-kdf
 t=$prefix/usr/bin/$n
-install -m 755 -D temp/$n $prefix/usr/bin/$n
+exit_on_error install -m 755 -D temp/$n $prefix/usr/bin/$n
